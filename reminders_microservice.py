e5a5466932601aa9bd1043101d046c3da53ae967
Patrick Godfrey <godfrepa@oregonstate.edu>
2023-02-13 20:31:54 +0900

    Implemented login, removed access to backend

--- reminders_microservice.py
+++ reminders_microservice.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import requests
+import argon2
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -12,44 +12,57 @@ cred = credentials.Certificate('.backendw2023-firebase-adminsdk-vjhgp-0f8c1203ba
 firebaseapp = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-app = Flask(__name__)
+# Initiliaze argon2 password hasher
+hasher = argon2.PasswordHasher()
 
-@app.route('/')
-def hello_world():
-    return {'message': 'Hello, World!'}
+def hash_password(password):
+    return hasher.hash(password)
 
-# @app.route("/sharereminders", methods=["POST"])
-# def getreminders2():
-#     data = request.get_json()
-#     user_ref = db.collection(u'users').document(data["email"])
-#     reminder_userID = user_ref.get().to_dict()["user_id"]
-#     ### Get the user's email from handshake service ###
-#     payload = {"r_user_id": reminder_userID}
-#     r = requests.get("http://localhost:8002", json=payload)
-# 
-# 
-#     ### Get the user's reminders from the database ###
-#     reminders_ref = user_ref.collection(u'reminders')
-#     reminders = []
-#     for reminder in reminders_ref.stream():
-#         reminders.append(reminder.to_dict())
-#     return jsonify(reminders), 200
+def verify_password(hashed_password, password):
+    try:
+        return hasher.verify(hashed_password, password)
+    except argon2.exceptions.VerifyMismatchError:
+        return False
 
 @app.route("/handshake", methods=["POST"])
 def handshake():
+    # Get the JSON data from the request
     data = request.get_json()
-    print(data)
-    r = requests.post("http://localhost:8000/login", json=data)
-    message = r.json()
-    if r.status_code == 200:
+    
+    # Check if the email exists
+    collection = db.collection('users')
+    doc_ref = collection.document(data['email'])
+    doc = doc_ref.get()
+    if doc.exists:
+        db_dict = doc.to_dict()
+        if verify_password(db_dict['password'], data['password']):
+            response = jsonify({
+                "message": "Login Successful",
+            })
+            response.status_code = 200
+        else:
+            response = jsonify({
+                "message": "Wrong password",
+            })
+            response.status_code = 401
+    else:
+        response = jsonify({
+            "message": "Email does not exist",
+        })
+        response.status_code = 401
+
+    # If login is successful, proceed with handshake
+    if response.status_code == 200:
         r_user_id_ref = db.collection(u'users').document(data["email"]).get()
         r_user_id = r_user_id_ref.to_dict()["user_id"]
         c_user_id = data['c_user_id']
-        handshake_db = db.collection(u'handshake.db')
-        handshake_db.add({"r_user_id": r_user_id, "c_user_id": c_user_id})
-        return message, 201
+        if not db.collection(u'handshake.db').where(u'r_user_id', u'==', r_user_id).where(u'c_user_id', u'==', c_user_id).get():
+            handshake_db = db.collection(u'handshake.db')
+            handshake_db.add({"r_user_id": r_user_id, "c_user_id": c_user_id})
+            response.status = 201
+        return response
     else:
-        return message, 404
+        return response
 
 
 @app.route("/getreminders", methods=["GET"])
@@ -75,6 +88,17 @@ def getreminders():
     reminders = []
     for reminder in user_reminders_ref.stream():
         r = reminder.to_dict()
+        
+        # Convert the "deadline" to two seperate fields, "date" and "time"
+        date_time = r["deadline"].split(" ")
+        r["date"], r["time"] = date_time[0], date_time[1]
+
+        # Remove unecessary fields
+        del r["deadline"]
+        del r["email"]
+        del r["description"]
+
+        # Add the reminder to the list
         reminders.append(r)
     return jsonify(reminders), 200
 
